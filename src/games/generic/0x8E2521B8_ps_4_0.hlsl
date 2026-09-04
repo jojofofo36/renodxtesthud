eca05e2a3791cc05c0eaee844dcf1e407d6013e8
jojofofo36 <114771853+jojofofo36@users.noreply.github.com>
2026-09-04 03:50:34 +0200

    Add files via upload

---
+++ src/games/generic/0x8E2521B8_ps_4_0.hlsl
@@ -0,0 +1,43 @@
+// ---- Created with 3Dmigoto v1.3.2 on Thu Sep 03 22:42:32 2026
+
+#include "shared.h"
+
+Texture2D<float4> t0 : register(t0);
+
+SamplerState s0_s : register(s0);
+
+cbuffer cb0 : register(b0)
+{
+  float4 cb0[4];
+}
+
+void main(
+  float4 v0 : SV_POSITION0,
+  float4 v1 : COLOR0,
+  float4 v2 : TEXCOORD0,
+  float4 v3 : TEXCOORD1,
+  float4 v4 : TEXCOORD2,
+  out float4 o0 : SV_Target0)
+{
+  float4 r0,r1,r2;
+
+  r0.x = 255 * v1.w;
+  r0.x = round(r0.x);
+  r0.w = 0.00392156886 * r0.x;
+
+  r1.xyzw = t0.Sample(s0_s, v2.xy).xyzw;
+  r1.xyzw = cb0[3].xyzw + r1.xyzw;
+
+  r2.x = r0.w * r1.w + -0.00100000005;
+  r2.x = cmp(r2.x < 0);
+
+  if (r2.x != 0) discard;
+
+  r0.xyz = v1.xyz;
+  r0.xyzw = r1.xyzw * r0.xyzw;
+
+  o0.xyz = r0.xyz * r0.www;
+  o0.w = r0.w * RENODX_UI_OPACITY;
+
+  return;
+}
