6377e2d23b12a866024b0e711ec9f9a66b321e80
jojofofo36 <114771853+jojofofo36@users.noreply.github.com>
2026-09-04 03:46:47 +0200

    Clean up commented definitions in shared.h
    Removed commented out tone mapping and color grading definitions from shared.h.

--- src/games/generic/shared.h
+++ src/games/generic/shared.h
@@ -1,39 +1,6 @@
 #ifndef SRC_TEMPLATE_SHARED_H_
 #define SRC_TEMPLATE_SHARED_H_
 
-// #define RENODX_PEAK_WHITE_NITS                 1000.f
-// #define RENODX_DIFFUSE_WHITE_NITS              renodx::color::bt2408::REFERENCE_WHITE
-// #define RENODX_GRAPHICS_WHITE_NITS             renodx::color::bt2408::GRAPHICS_WHITE
-// #define RENODX_COLOR_GRADE_STRENGTH            1.f
-// #define RENODX_TONE_MAP_TYPE                   TONE_MAP_TYPE_RENO_DRT
-// #define RENODX_TONE_MAP_EXPOSURE               1.f
-// #define RENODX_TONE_MAP_HIGHLIGHTS             1.f
-// #define RENODX_TONE_MAP_SHADOWS                1.f
-// #define RENODX_TONE_MAP_CONTRAST               1.f
-// #define RENODX_TONE_MAP_SATURATION             1.f
-// #define RENODX_TONE_MAP_HIGHLIGHT_SATURATION   1.f
-// #define RENODX_TONE_MAP_BLOWOUT                0
-// #define RENODX_TONE_MAP_FLARE                  0
-// #define RENODX_TONE_MAP_HUE_CORRECTION         1.f
-// #define RENODX_TONE_MAP_HUE_SHIFT              0
-// #define RENODX_TONE_MAP_WORKING_COLOR_SPACE    color::convert::COLOR_SPACE_BT709
-// #define RENODX_TONE_MAP_CLAMP_COLOR_SPACE      color::convert::COLOR_SPACE_NONE
-// #define RENODX_TONE_MAP_CLAMP_PEAK             color::convert::COLOR_SPACE_BT709
-// #define RENODX_TONE_MAP_HUE_PROCESSOR          HUE_PROCESSOR_OKLAB
-// #define RENODX_TONE_MAP_PER_CHANNEL            0
-// #define RENODX_GAMMA_CORRECTION                GAMMA_CORRECTION_GAMMA_2_2
-// #define RENODX_INTERMEDIATE_SCALING            (RENODX_DIFFUSE_WHITE_NITS / RENODX_GRAPHICS_WHITE_NITS)
-// #define RENODX_INTERMEDIATE_ENCODING           (RENODX_GAMMA_CORRECTION + 1.f)
-// #define RENODX_INTERMEDIATE_COLOR_SPACE        color::convert::COLOR_SPACE_BT709
-// #define RENODX_SWAP_CHAIN_DECODING             RENODX_INTERMEDIATE_ENCODING
-// #define RENODX_SWAP_CHAIN_DECODING_COLOR_SPACE RENODX_INTERMEDIATE_COLOR_SPACE
-// #define RENODX_SWAP_CHAIN_CUSTOM_COLOR_SPACE   COLOR_SPACE_CUSTOM_BT709D65
-// #define RENODX_SWAP_CHAIN_SCALING_NITS         RENODX_GRAPHICS_WHITE_NITS
-// #define RENODX_SWAP_CHAIN_CLAMP_NITS           RENODX_PEAK_WHITE_NITS
-// #define RENODX_SWAP_CHAIN_CLAMP_COLOR_SPACE    color::convert::COLOR_SPACE_UNKNOWN
-// #define RENODX_SWAP_CHAIN_ENCODING             ENCODING_SCRGB
-// #define RENODX_SWAP_CHAIN_ENCODING_COLOR_SPACE color::convert::COLOR_SPACE_BT709
-
 // Must be 32bit aligned
 // Should be 4x32
 struct ShaderInjectData {
@@ -63,10 +30,7 @@ struct ShaderInjectData {
   float intermediate_color_space;
   float swap_chain_decoding;
   float swap_chain_gamma_correction;
-  //  float swap_chain_decoding_color_space;
   float swap_chain_custom_color_space;
-  // float swap_chain_scaling_nits;
-  // float swap_chain_clamp_nits;
   float swap_chain_clamp_color_space;
   float swap_chain_encoding;
   float swap_chain_encoding_color_space;
@@ -75,50 +39,22 @@ struct ShaderInjectData {
 };
 
 #ifndef __cplusplus
+
+// NOTE: on ne branche QUE le cbuffer d'injection ici, sans tirer toute la
+// pipeline de tone mapping RenoDX (renodx.hlsl). Ce mod ne fait que de
+// l'opacite HUD, et renodx.hlsl (pense pour du SM5+) casse la compilation
+// des shaders ps_4_0 du jeu qui n'en ont pas besoin.
+
 #if ((__SHADER_TARGET_MAJOR == 5 && __SHADER_TARGET_MINOR >= 1) || __SHADER_TARGET_MAJOR >= 6)
 cbuffer shader_injection : register(b13, space50) {
-#elif (__SHADER_TARGET_MAJOR < 5) || ((__SHADER_TARGET_MAJOR == 5) && (__SHADER_TARGET_MINOR < 1))
+#else
 cbuffer shader_injection : register(b13) {
 #endif
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
-#define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
-#define RENODX_PEAK_WHITE_NITS               shader_injection.peak_white_nits
-#define RENODX_DIFFUSE_WHITE_NITS            shader_injection.diffuse_white_nits
-#define RENODX_GRAPHICS_WHITE_NITS           shader_injection.graphics_white_nits
-#define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
-#define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.tone_map_per_channel
-#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  shader_injection.tone_map_working_color_space
-#define RENODX_TONE_MAP_HUE_PROCESSOR        shader_injection.tone_map_hue_processor
-#define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.tone_map_hue_correction
-#define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.tone_map_hue_shift
-#define RENODX_TONE_MAP_CLAMP_COLOR_SPACE    shader_injection.tone_map_clamp_color_space
-#define RENODX_TONE_MAP_CLAMP_PEAK           shader_injection.tone_map_clamp_peak
-#define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
-#define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
-#define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
-#define RENODX_TONE_MAP_CONTRAST             shader_injection.tone_map_contrast
-#define RENODX_TONE_MAP_SATURATION           shader_injection.tone_map_saturation
-#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.tone_map_highlight_saturation
-#define RENODX_TONE_MAP_BLOWOUT              shader_injection.tone_map_blowout
-#define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
-#define RENODX_COLOR_GRADE_STRENGTH          shader_injection.color_grade_strength
-#define RENODX_INTERMEDIATE_ENCODING         shader_injection.intermediate_encoding
-#define RENODX_SWAP_CHAIN_DECODING           shader_injection.swap_chain_decoding
-#define RENODX_SWAP_CHAIN_GAMMA_CORRECTION   shader_injection.swap_chain_gamma_correction
-// #define RENODX_SWAP_CHAIN_DECODING_COLOR_SPACE shader_injection.swap_chain_decoding_color_space
-#define RENODX_SWAP_CHAIN_CUSTOM_COLOR_SPACE shader_injection.swap_chain_custom_color_space
-// #define RENODX_SWAP_CHAIN_SCALING_NITS         shader_injection.swap_chain_scaling_nits
-// #define RENODX_SWAP_CHAIN_CLAMP_NITS           shader_injection.swap_chain_clamp_nits
-#define RENODX_SWAP_CHAIN_CLAMP_COLOR_SPACE    shader_injection.swap_chain_clamp_color_space
-#define RENODX_SWAP_CHAIN_ENCODING             shader_injection.swap_chain_encoding
-#define RENODX_SWAP_CHAIN_ENCODING_COLOR_SPACE shader_injection.swap_chain_encoding_color_space
-#define RENODX_UI_OPACITY                      shader_injection.ui_opacity
-#define RENODX_RENO_DRT_TONE_MAP_METHOD        renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+#define RENODX_UI_OPACITY shader_injection.ui_opacity
 
-#include "../../shaders/renodx.hlsl"
-
-#endif
+#endif  // __cplusplus
 
 #endif  // SRC_TEMPLATE_SHARED_H_
